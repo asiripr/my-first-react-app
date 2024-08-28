b573403c0806bd7569abea0640174ee611e785c7
asiripr <asiripramodaya2000@gmail.com>
2024-08-28 15:45:00 +0530

    started to work with components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import './App.css';
 
 function App() {
-  const userName = "Gamage";
-  const description = (
-    <div>
-      <h2>School</h2>
-      <p>My school is Morawaka Kanishta Vidyalaya</p>
-    </div>
-  );
+  // const userName = "Gamage";
+  // const description = (
+  //   <div>
+  //     <h2>School</h2>
+  //     <p>My school is Morawaka Kanishta Vidyalaya</p>
+  //   </div>
+  // );
   return (
+    // <div>
+    //   <h2>This is Asiri</h2>
+    //   <p>Hi World. Asiri Started to learn React</p>
+    //   {/* /<h3>userName</h3> we cant use variables in the code like this*/}
+    //   {/* we can use following methodology */}
+    //   <h3>{userName}</h3>
+    //   {description}
+    // </div>
     <div>
-      <h2>This is Asiri</h2>
-      <p>Hi World. Asiri Started to learn React</p>
-      {/* /<h3>userName</h3> we cant use variables in the code like this*/}
-      {/* we can use following methodology */}
-      <h3>{userName}</h3>
-      {description}
+      <UserData />
+      <UserData />
+      <UserData />
+      <UserData />
     </div>
   );
 }
-
+// UserData component
+const UserData = () =>{
+  return(
+    <div>
+      <h2>Sabaragamuwa</h2>
+      <p>Computing</p>
+    </div>
+  );
+}; 
 export default App;
