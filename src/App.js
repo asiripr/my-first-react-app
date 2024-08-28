e1a83c9d0e45ba391904596a50b312513f114605
asiripr <asiripramodaya2000@gmail.com>
2024-08-28 18:10:07 +0530

    modified UserLink for use props

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ function App() {
       <UserData userName="Sahan Randunu" userEmail="randunu@gmail.com"/>
       <UserData userName="Kavmini Thathsarani" userEmail="kthathsarani@gmail.com"/>
       <UserData userName="Hasira Manoj" userEmail="mhasira@gmail.com"/>
+
+      
+      <UserLink link="https://github.com/asiripr" label="Asiri's GitHub"/>
+      <UserLink link="https://x.com/asiripramodaya" label="Asiri's X"/>
       
     </div>
   );
@@ -23,10 +27,10 @@ const UserData = (props) =>{
 }; 
 
 // another user componet
-const UserLink = () => {
+const UserLink = (props) => {
   return(
     <div>
-      <a href='https://github.com/asiripr' target='_blank'>Asiri's GitHub</a>
+      <a href={props.link} target='_blank'>{props.label}</a>
     </div>
   );
 }
