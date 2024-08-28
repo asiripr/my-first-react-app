a670bff492e6ee6509ff59faf02fd703e08cee3c
asiripr <asiripramodaya2000@gmail.com>
2024-08-28 18:00:49 +0530

    modified UserData for use props

--- src/App.js
+++ src/App.js
@@ -3,20 +3,21 @@ import './App.css';
 function App() {
   return (
     <div>
-      <UserData />
-      <UserData />
-      <UserLink />
-      <UserData />
-      <UserData />
+      <UserData userName="Pawani Jayalath" userEmail="pawani@gmail.com"/>
+      <UserData userName="Hasun Sithmika" userEmail="sithmika@gmail.com"/>
+      <UserData userName="Sahan Randunu" userEmail="randunu@gmail.com"/>
+      <UserData userName="Kavmini Thathsarani" userEmail="kthathsarani@gmail.com"/>
+      <UserData userName="Hasira Manoj" userEmail="mhasira@gmail.com"/>
+      
     </div>
   );
 }
 // UserData component
-const UserData = () =>{
+const UserData = (props) =>{
   return(
     <div>
-      <h2>Sabaragamuwa</h2>
-      <p>Computing</p>
+      <h2>{props.userName}</h2>
+      <p>{props.userEmail}</p>
     </div>
   );
 }; 
@@ -25,7 +26,7 @@ const UserData = () =>{
 const UserLink = () => {
   return(
     <div>
-      <a href='https://github.com/asiripr'>Asiri's GitHub</a>
+      <a href='https://github.com/asiripr' target='_blank'>Asiri's GitHub</a>
     </div>
   );
 }
