cc8a639daa020a61bccabf51c6ab066e69b86efd
asiripr <asiripramodaya2000@gmail.com>
2024-08-28 11:24:50 +0530

    how to work with variables on jsx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
 
 function App() {
+  const userName = "Gamage";
+  const description = (
+    <div>
+      <h2>School</h2>
+      <p>My school is Morawaka Kanishta Vidyalaya</p>
+    </div>
+  );
   return (
     <div>
       <h2>This is Asiri</h2>
       <p>Hi World. Asiri Started to learn React</p>
+      {/* /<h3>userName</h3> we cant use variables in the code like this*/}
+      {/* we can use following methodology */}
+      <h3>{userName}</h3>
+      {description}
     </div>
   );
 }
