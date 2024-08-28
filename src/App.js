12a507d8460504ef81a1b3b76d19e355ca67cbfe
asiripr <asiripramodaya2000@gmail.com>
2024-08-28 10:50:31 +0530

    did some excersices

--- src/App.js
+++ src/App.js
@@ -2,21 +2,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>This is Asiri Pramodaya</h1>
-        <p>
-          I am going to learn <code>REACT JS</code>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>This is Asiri</h2>
+      <p>Hi World. Asiri Started to learn React</p>
     </div>
   );
 }
